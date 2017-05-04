6f9d1738e2151e12a647d6fc314842042bcba39e
Benjamin Devillers <bdvllrs@gmail.com>
2017-05-04 16:34:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from models.Example import Example
 if __name__ == '__main__':
     CONFIG = API_CONFIG('http://localhost/api')
 
-    exmaple = Example(CONFIG)
-    print(exmaple.put(_id=1, mane='test'))
+    example = Example(CONFIG)
+    print(example.put(_id=1, mane='test'))
